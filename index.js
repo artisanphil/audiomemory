c3e70abcc286e8986199743827b1eca61abcf9ae
Philip Perry <philip.perry@learnclick.com>
2022-03-12 15:31:28 +0000

    concat audio output into one file

--- index.js
+++ index.js
@@ -7,26 +7,22 @@ async function main() {
   
   let text = "The Lord is my shepherd, I lack nothing. ";
     text += "He makes me lie down in green pastures, he leads me beside quiet waters, he refreshes my soul. "
-    text += "He guides me along the right paths for his name’s sake. ";
-    // text += "Even though I walk through the darkest valley, I will fear no evil, for you are with me; "
-    // text += "your rod and your staff, they comfort me. "
-    // text += "You prepare a table before me in the presence of my enemies. "
-    // text += "You anoint my head with oil; my cup overflows. ";
-    // text += "Surely your goodness and love will follow me all the days of my life, "
-    // text += "and I will dwell in the house of the Lord forever.";
+    text += "He guides me along the right paths for his name’s seik. "
+    text += "Even though I walk through the darkest valley, I will fear no evil, for you are with me; "
+    text += "your rod and your staff, they comfort me. "
+    text += "You prepare a table before me in the presence of my enemies. "
+    text += "You anoint my head with oil; my cup overflows. ";
+    text += "Surely your goodness and love will follow me all the days of my life, "
+    text += "and I will dwell in the house of the Lord forever.";
   let textWithGaps = await TextToGaps.process(text, ['NOUN', 'ADJ']);
   textWithGaps += '<break time="2s"/>';
-  audioContent = await TextToMP3.convert(textWithGaps);
+  
+  textWithGaps += await TextToGaps.process(text, ['NOUN', 'ADJ', 'VERB']);
+  textWithGaps += '<break time="2s"/>';
 
-  // textWithGaps = await TextToGaps.process(text, ['NOUN', 'ADJ', 'VERB']);
-  // textWithGaps += '<break time="2s"/>';
-  // audioContent += await TextToMP3.convert(textWithGaps);
-
-  // textWithGaps = await TextToGaps.process(text, ['NOUN', 'ADJ', 'VERB', 'PRON']);
-  // audioContent += await TextToMP3.convert(textWithGaps);
-
-  // const writeFile = util.promisify(fs.writeFile);
-  // await writeFile('output.mp3', audioContent, 'binary');
+  textWithGaps += await TextToGaps.process(text, ['NOUN', 'ADJ', 'VERB', 'PRON']);
+  
+  await TextToMP3.convert(textWithGaps);
 
   console.log(textWithGaps);
 }
--- texttomp3.js
+++ texttomp3.js
@@ -1,29 +1,55 @@
 module.exports = {
   convert: async (input) => {
     const textToSpeech = require('@google-cloud/text-to-speech');
-
+    const textSynthesize =require('google-text-to-speech-concat');
+    
     // Import other required libraries
     const fs = require('fs');
-    const util = require('util');
-    // Creates a client
-    const client = new textToSpeech.TextToSpeechClient();    
-    // The text to synthesize
-    const text = '<speak>' + input + '</speak>';
-    console.log('text length: ' + text.length);
-
-    // Construct the request
-    const request = {
-      input: {ssml: text},
-      // Select the language and SSML voice gender (optional)
-      voice: {languageCode: 'en-GB', name:'en-GB-Standard-B', ssmlGender: 'MALE'},
-      // select the type of audio encoding
-      audioConfig: {audioEncoding: 'MP3'},
-    };
-
-    // Performs the text-to-speech request
-    const [response] = await client.synthesizeSpeech(request);
-    // Write the binary audio content to a local file
+    const path = require('path');
     
-    return response.audioContent;
+    try {
+      // Creates a client
+      const client = new textToSpeech.TextToSpeechClient();    
+      // The text to synthesize
+      const text = '<speak>' + input + '</speak>';
+      console.log('text length: ' + text.length);
+
+      // Construct the request
+      const request = {
+        input: {ssml: text},
+        // Select the language and SSML voice gender (optional)
+        voice: {languageCode: 'en-GB', name:'en-GB-Standard-B', ssmlGender: 'MALE'},
+        // select the type of audio encoding
+        audioConfig: {audioEncoding: 'MP3'},
+      };
+
+      // Synthesize the text, resulting in an audio buffer
+      const buffer = await textSynthesize.synthesize(client, request);
+  
+      // Handle the buffer
+      // For example write it to a file or directly upload it to storage, like S3 or Google Cloud Storage
+      const outputFile = path.join(__dirname, 'output.mp3');
+
+      fs.writeFile(outputFile, buffer, 'binary', (err) => {
+          if (err) throw err;
+          console.log('Got audio!', outputFile);
+        });
+    } catch (err) {
+      console.log(err);
+    }     
+  
+     // Write the file
+  //    fs.writeFile(outputFile, buffer, 'binary', (err) => {
+  //      if (err) throw err;
+  //      console.log('Got audio!', outputFile);
+  //    });
+  //  } catch (err) {
+  //    console.log(err);
+  //  }
+
+  //   // Performs the text-to-speech request
+  //   const [response] = await client.synthesizeSpeech(request);
+    
+  //   return response.audioContent;
   }
 }
