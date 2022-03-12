697fd7cf387c6f48c8066babd801710c3afd6d15
Philip Perry <philip.perry@learnclick.com>
2022-03-12 17:42:00 +0000

    splitting text into batches of max 5000 characters and concatinating mp3 files to one

--- index.js
+++ index.js
@@ -1,30 +1,74 @@
 async function main() {
-  const fs = require('fs');
-  const util = require('util');
+  const TextToGaps = require("./texttogaps");
+  const TextToMP3 = require("./texttomp3");
+  const SSMLSplit = require("ssml-split");
+  const audioconcat = require("audioconcat");
+  const fs = require("fs");
+  const util = require("util");
 
-  const TextToGaps = require('./texttogaps');
-  const TextToMP3 = require('./texttomp3');
-  
   let text = "The Lord is my shepherd, I lack nothing. ";
-    text += "He makes me lie down in green pastures, he leads me beside quiet waters, he refreshes my soul. "
-    text += "He guides me along the right paths for his name’s seik. "
-    text += "Even though I walk through the darkest valley, I will fear no evil, for you are with me; "
-    text += "your rod and your staff, they comfort me. "
-    text += "You prepare a table before me in the presence of my enemies. "
-    text += "You anoint my head with oil; my cup overflows. ";
-    text += "Surely your goodness and love will follow me all the days of my life, "
-    text += "and I will dwell in the house of the Lord forever.";
-  let textWithGaps = await TextToGaps.process(text, ['NOUN', 'ADJ']);
-  textWithGaps += '<break time="2s"/>';
-  
-  textWithGaps += await TextToGaps.process(text, ['NOUN', 'ADJ', 'VERB']);
+  text +=
+    "He makes me lie down in green pastures, he leads me beside quiet waters, he refreshes my soul. ";
+  text += "He guides me along the right paths for his name’s seik. ";
+  text +=
+    "Even though I walk through the darkest valley, I will fear no evil, for you are with me; ";
+  text += "your rod and your staff, they comfort me. ";
+  text += "You prepare a table before me in the presence of my enemies. ";
+  text += "You anoint my head with oil; my cup overflows. ";
+  text +=
+    "Surely your goodness and love will follow me all the days of my life, ";
+  text += "and I will dwell in the house of the Lord forever.";
+  let textWithGaps = await TextToGaps.process(text, ["NOUN", "ADJ"]);
   textWithGaps += '<break time="2s"/>';
 
-  textWithGaps += await TextToGaps.process(text, ['NOUN', 'ADJ', 'VERB', 'PRON']);
-  
-  await TextToMP3.convert(textWithGaps);
+  textWithGaps += await TextToGaps.process(text, ["NOUN", "ADJ", "VERB"]);
+  textWithGaps += '<break time="2s"/>';
 
-  console.log(textWithGaps);
+  textWithGaps += await TextToGaps.process(text, [
+    "NOUN",
+    "ADJ",
+    "VERB",
+    "PRON",
+  ]);
+
+  const ssmlSplit = new SSMLSplit.default({
+    // The service you are using: "google" or "aws"
+    synthesizer: "google",
+    // Finds a possible split moment starting from 4000 characters
+    softLimit: 4000,
+    // Google Text to Speech limitation
+    hardLimit: 5000,
+    // Allow to split large paragraphs, set to false to keep your <p></p> intact
+    breakParagraphsAboveHardLimit: true,
+  });
+
+  const batches = ssmlSplit.split(textWithGaps);
+
+  let allMp3s = [];
+  for (let batchNr = 0; batchNr < batches.length; batchNr++) {
+    console.log(batches[batchNr]);
+    audioContent = await TextToMP3.convert(batches[batchNr], batchNr);
+
+    const writeFile = util.promisify(fs.writeFile);
+    mp3File = "output/" + batchNr + ".mp3";
+    allMp3s.push(mp3File);
+    await writeFile(mp3File, audioContent, "binary");
+  }
+
+  audioconcat(allMp3s)
+    .concat("output.mp3")
+    .on("start", function (command) {
+      console.log("ffmpeg process started:", command);
+    })
+    .on("error", function (err, stdout, stderr) {
+      console.error("Error:", err);
+      console.error("ffmpeg stderr:", stderr);
+    })
+    .on("end", function (output) {
+      console.error("Audio created in:", output);
+    });
+
+  //console.log(textWithGaps);
 }
 
 main(...process.argv.slice(2));
--- texttomp3.js
+++ texttomp3.js
@@ -1,55 +1,25 @@
-module.exports = {
-  convert: async (input) => {
+module.exports = {    
+  convert: async (text, batchNr) => {
     const textToSpeech = require('@google-cloud/text-to-speech');
-    const textSynthesize =require('google-text-to-speech-concat');
     
-    // Import other required libraries
-    const fs = require('fs');
-    const path = require('path');
-    
-    try {
-      // Creates a client
-      const client = new textToSpeech.TextToSpeechClient();    
-      // The text to synthesize
-      const text = '<speak>' + input + '</speak>';
-      console.log('text length: ' + text.length);
+    // Creates a client
+    const client = new textToSpeech.TextToSpeechClient();    
+    // The text to synthesize
+    console.log('text length: ' + text.length);
 
-      // Construct the request
-      const request = {
-        input: {ssml: text},
-        // Select the language and SSML voice gender (optional)
-        voice: {languageCode: 'en-GB', name:'en-GB-Standard-B', ssmlGender: 'MALE'},
-        // select the type of audio encoding
-        audioConfig: {audioEncoding: 'MP3'},
-      };
+    // Construct the request
+    const request = {
+      input: {ssml: text},
+      // Select the language and SSML voice gender (optional)
+      voice: {languageCode: 'en-GB', name:'en-GB-Standard-F', ssmlGender: 'MALE'},
+      // select the type of audio encoding
+      audioConfig: {audioEncoding: 'MP3'},
+    };
 
-      // Synthesize the text, resulting in an audio buffer
-      const buffer = await textSynthesize.synthesize(client, request);
-  
-      // Handle the buffer
-      // For example write it to a file or directly upload it to storage, like S3 or Google Cloud Storage
-      const outputFile = path.join(__dirname, 'output.mp3');
+    // Performs the text-to-speech request
+    const [response] = await client.synthesizeSpeech(request);
+    // Write the binary audio content to a local file
 
-      fs.writeFile(outputFile, buffer, 'binary', (err) => {
-          if (err) throw err;
-          console.log('Got audio!', outputFile);
-        });
-    } catch (err) {
-      console.log(err);
-    }     
-  
-     // Write the file
-  //    fs.writeFile(outputFile, buffer, 'binary', (err) => {
-  //      if (err) throw err;
-  //      console.log('Got audio!', outputFile);
-  //    });
-  //  } catch (err) {
-  //    console.log(err);
-  //  }
-
-  //   // Performs the text-to-speech request
-  //   const [response] = await client.synthesizeSpeech(request);
-    
-  //   return response.audioContent;
+    return response.audioContent;    
   }
 }
