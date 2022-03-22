ce465decca306b9cede2a388899489383bd0dc70
Philip Perry <philip.perry@learnclick.com>
2022-03-22 20:15:08 +0000

    read text from file

--- index.js
+++ index.js
@@ -5,23 +5,12 @@ async function main() {
   const TextToGaps = require("./texttogaps");
   const fs = require('fs');
   
-  let text = "The Lord is my shepherd, I lack nothing.\n ";
-  text +=
-    "He makes me lie down in green pastures,\n he leads me beside quiet waters,\n he refreshes my soul.\n ";
-  text += "He guides me along the right paths\n for his name’s seik.\n ";
-  text +=
-    "Even though I walk through the darkest valley,\n I will fear no evil, for you are with me;\n ";
-  text += "your rod and your staff,\n they comfort me.\n ";
-  text += "You prepare a table before me\n in the presence of my enemies.\n ";
-  text += "You anoint my head with oil;\n my cup overflows.\n ";
-  text +=
-    "Surely your goodness and love will follow me\n all the days of my life,\n ";
-  text += "and I will dwell in the house of the Lord forever.";
+  const text = fs.readFileSync('./input.txt', {encoding:'utf8', flag:'r'});
 
   let textBatches = SplitText.split(text);
   await SaveAudio.removeFilesInDirectory("output");    
   await SaveAudio.removeFilesInDirectory("temporary");    
-  //await SaveAudio.createFolder("output");
+
   while(await !fs.existsSync('output')) {
     await SaveAudio.createFolder("output");
   }
